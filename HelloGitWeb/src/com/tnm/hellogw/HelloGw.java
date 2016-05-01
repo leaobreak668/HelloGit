cac53b86f798fa002193ca0f68df49d917877a86
leaobreak668 <mvcliao@foxmail.com>
2016-05-01 16:27:20 +0800

    dd

--- HelloGitWeb/src/com/tnm/hellogw/HelloGw.java
+++ HelloGitWeb/src/com/tnm/hellogw/HelloGw.java
@@ -4,7 +4,7 @@ public class HelloGw {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello...Git");
+		System.out.println("Hello...GitHub");
 	}
 
 }
