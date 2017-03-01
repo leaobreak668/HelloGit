ab781465d88c0186fde3911a9da76d52c32e0fda
leaobreak668 <mvcliao@foxmail.com>
2017-03-02 01:11:16 +0800

    gdgfd

--- HelloGitWeb/src/com/tnm/hellogw/HelloGw.java
+++ HelloGitWeb/src/com/tnm/hellogw/HelloGw.java
@@ -8,11 +8,6 @@ public class HelloGw {
 		System.out.println("Hello Github");
 		//难首这里也要我解释一下吗，我真是搞不懂，这有什么好解释的。
 		//也不知道，为什么有的时候行，有的时候不行呢。
-		//靠
-		//abcdefgf
-		//1
-		//fdsfsf
-		//23
 		//fuck your br19999999999999999999
 		//尼码啊
 	}
