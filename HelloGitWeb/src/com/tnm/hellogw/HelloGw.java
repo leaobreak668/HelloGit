24322fa5c83c0d9f3f7fff9f267e002c086ad318
leaobreak668 <mvcliao@foxmail.com>
2017-03-01 22:41:16 +0800

    d

--- HelloGitWeb/src/com/tnm/hellogw/HelloGw.java
+++ HelloGitWeb/src/com/tnm/hellogw/HelloGw.java
@@ -6,6 +6,9 @@ public class HelloGw {
 		// TODO Auto-generated method stub
 		System.out.println("Hello...GitHub");
 		System.out.println("Hello Github");
+		//难首这里也要我解释一下吗，我真是搞不懂，这有什么好解释的。
+		//也不知道，为什么有的时候行，有的时候不行呢。
+		//靠
 	}
 
 }
