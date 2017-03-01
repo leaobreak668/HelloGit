fd479800e1a5c2acc116844768194d335e09f87c
leaobreak668 <mvcliao@foxmail.com>
2017-03-02 01:16:59 +0800

    fd

--- HelloGitWeb/src/com/tnm/hellogw/HelloGw.java
+++ HelloGitWeb/src/com/tnm/hellogw/HelloGw.java
@@ -7,7 +7,7 @@ public class HelloGw {
 		System.out.println("Hello...GitHub");
 		System.out.println("Hello Github");
 		//难首这里也要我解释一下吗，我真是搞不懂，这有什么好解释的。
-		//也不知道，为什么有的时候行，有的时候不行呢。
+		//也不知道，为什么有的kkdlfjs=============时候行，有的时候不行呢。
 		//fuck your br19999999999999999999
 		//尼码啊fdsfdsfdsf
 		//dfsd
